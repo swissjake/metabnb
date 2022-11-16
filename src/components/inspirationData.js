0a1d9f6cab3afa41edaddf45a10a74e3dda9fd6a
SOFIRITARI AMGBARA <sofiriamgbara@gmail.com>
2022-11-16 07:19:00 +0100

    image correction

--- src/components/inspirationData.js
+++ src/components/inspirationData.js
@@ -1,10 +1,10 @@
-import img1 from "../assets/inspiration/img1.svg";
-import img2 from "../assets/inspiration/img2.svg";
-import img3 from "../assets/inspiration/img3.svg";
-import img4 from "../assets/inspiration/img4.svg";
-import img5 from "../assets/inspiration/img5.svg";
-import img6 from "../assets/inspiration/img6.svg";
-import img7 from "../assets/inspiration/img7.svg";
-import img8 from "../assets/inspiration/img8.svg";
+import img1 from "../assets/inspiration/img1.png";
+import img2 from "../assets/inspiration/img2.png";
+import img3 from "../assets/inspiration/img3.png";
+import img4 from "../assets/inspiration/img4.png";
+import img5 from "../assets/inspiration/img5.png";
+import img6 from "../assets/inspiration/img6.png";
+import img7 from "../assets/inspiration/img7.png";
+import img8 from "../assets/inspiration/img8.png";
 
 export const data = [img1, img2, img3, img4, img5, img6, img7, img8];
